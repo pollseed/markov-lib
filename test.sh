c096f27fc828c0acfa17db997f77629dd1e8c226
pollseed <cb.so.devope+0001@gmail.com>
2016-01-20 00:08:57 +0900

    Add pm2 #13

--- test.sh
+++ test.sh
@@ -4,8 +4,8 @@ readonly PORT=3000
 readonly HTTP_STATUS='%{http_code}\n'
 readonly SUCCESS=200
 
-function npm_start() {
-  npm start $1$2>/dev/null
+function pm2_start() {
+  pm2 start bin/www $1$2>/dev/null
   sleep 1
 }
 
@@ -13,6 +13,6 @@ function error_check() {
   curl -LI localhost:${PORT} -o /dev/null -w ${HTTP_STATUS} -s | grep ${SUCCESS} $1$2>/dev/null
 }
 
-npm_start
+pm2_start
 error_check
 echo $?
