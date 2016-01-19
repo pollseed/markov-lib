74c2b712b7b74da469fe79b89089982e21e3f427
pollseed <cb.so.devope+0001@gmail.com>
2016-01-19 23:59:05 +0900

    Update Permission test.sh #13

--- test.sh
+++ test.sh
@@ -4,15 +4,15 @@ readonly PORT=3000
 readonly HTTP_STATUS='%{http_code}\n'
 readonly SUCCESS=200
 
-function pm2_start() {
-  npm start bin/www $2>/dev/null
+function npm_start() {
+  npm start $1$2>/dev/null
   sleep 1
 }
 
 function error_check() {
-  curl -LI localhost:${PORT} -o /dev/null -w ${HTTP_STATUS} -s | grep ${SUCCESS} $2>/dev/null
+  curl -LI localhost:${PORT} -o /dev/null -w ${HTTP_STATUS} -s | grep ${SUCCESS} $1$2>/dev/null
 }
 
-pm2_start
+npm_start
 error_check
 echo $?
